56ee1d6e1f702ec2065659510d8c56f7dd6167ad
dilip.chandolu <dilip.chandolu@hotmail.com>
2018-01-25 21:41:58 +0000

    Added new files

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { DropzoneModule, DropzoneConfigInterface, DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { UploadComponent } from './upload/upload.component'
+
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+    // Change this to your upload POST address:
+    url: 'https://httpbin.org/post',
+    acceptedFiles: 'image/*',
+    createImageThumbnails: true
+};
+
+@NgModule({
+  declarations: [
+      AppComponent,
+      UploadComponent,
+  ],
+  imports: [
+      BrowserModule,
+      DropzoneModule
+  ],
+  providers: [
+      {
+          provide: DROPZONE_CONFIG,
+          useValue: DEFAULT_DROPZONE_CONFIG
+      }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,51 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { DropzoneComponent, DropzoneDirective, DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+
+
+
+export class UploadComponent implements OnInit {
+
+    public type: string = 'component';
+    public disabled: boolean = false;
+    public config: DropzoneConfigInterface = {
+        clickable: true,
+        maxFiles: 5,
+        autoReset: null,
+        errorReset: null,
+        cancelReset: null
+    };
+
+    @ViewChild(DropzoneComponent) componentRef: DropzoneComponent;
+    @ViewChild(DropzoneDirective) directiveRef: DropzoneDirective;
+
+  constructor() { }
+
+  
+  ngOnInit() {
+  }
+
+  public resetDropzoneUploads() {
+      if (this.type === 'directive') {
+          this.directiveRef.reset();
+      } else {
+          this.componentRef.directiveRef.reset();
+      }
+  }
+
+  public onUploadError(args: any) {
+      console.log('onUploadError:', args);
+
+      //This is to reset the upload control
+      this.resetDropzoneUploads();
+  }
+
+  public onUploadSuccess(args: any) {
+      console.log('onUploadSuccess:', args);
+  }
+}
